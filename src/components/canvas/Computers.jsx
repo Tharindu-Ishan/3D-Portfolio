59890af0122bd582baf9bbeefb112ebdef8f49eb
Tharindu Ishan <ishanthari96@gmail.com>
2023-11-14 10:11:45 +0530

    Make first page

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -10,13 +10,19 @@ const Computers = () => {
       groundColor="black"/>
       <pointLight intensity={1}/>
       <spotLight 
-        position={[-20,50,10]}
+        position={[-20,50,10]} 
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
       />
       <primitive 
         object={computer.scene}
         scale={0.75}
-        position={[0,-3.25,-1.5]}
+        position={[1,-2.25,-1.5]}
         rotation={[-0.01,-0.2,-0.1]}
+        
       />
     </mesh>
   )
@@ -25,7 +31,7 @@ const Computers = () => {
 const ComputersCanvas = ()=>{
   return(
     <Canvas 
-      frameLoop="demand"
+      fr="demand"
       shadows
       camera={{position:[20,3,5],fov:25}}
       gl={{preserveDrawingBuffer:true}}
