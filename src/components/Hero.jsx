6a0653dacd75606c2670e282d37bd7e955524c6a
Tharindu Ishan <ishanthari96@gmail.com>
2023-11-21 08:55:41 +0530

    Add swipe right gif

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,6 +4,12 @@ import { ComputersCanvas } from './canvas';
 
 
 const Hero = () => {
+  const swipe={
+    width: '200px',
+    
+    
+    
+  };
   return (
     <section className='relative w-full h-screen mx-auto'>
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl max-auto flex flex-row items-start gap-5`}>
@@ -16,11 +22,15 @@ const Hero = () => {
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
               Full Stack Developer <br className='sm:block hidden'/> B.Sc.Engineering (Hons).<br />University of Moratuwa.
           </p>
+        
         </div>
       </div>
       
       <ComputersCanvas />
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex flex-col justify-center items-center'>
+        <div>
+          <h1><img src="./src/assets/swipe/swipe.gif" alt="" style={swipe} draggable={false}/></h1>
+        </div>
         <a href="#about">
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div 
